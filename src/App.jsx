2031a7a84bc172b121a14515da2836d6b857ff9e
mitesh darda <mitesh.darda.0@gmail.com>
2024-12-28 16:43:56 +0530

    refact: add preetier

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Card from "./components/Card";
-import { ProductCategories } from "./components/ProductCategories";
+import Card from './components/Card';
+import { ProductCategories } from './components/ProductCategories';
 
 function App() {
   return (
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,19 +6,16 @@ function Intro() {
           Global Trading Distribution & Consulting
         </h2>
         <p className="text-[#747474] text-[17px] font-[500]">
-          Mudra Global has been formed to work progressively in the field of
-          import & export, business. We offer a secure platform to facilitate
-          the supply of quality products across domestic and foreign countries.
-          As an import & export. business, we seek to provide real services to
-          exporters and importers who are committed to the worldwide supply of
-          commodities around the entire world. We deal reasonably with
-          producers, farmers, wholesalers, importers, and other stakeholders to
-          establish a strong global presence in international trade right away.
-          Being an import export company, our final aspiration is to close the
-          gap in the foreign market between buyers and sellers. In order to
-          offer the highest quality of merchandise, ensure customer satisfaction
-          and cultivate customer loyalty, we guarantee assistance by qualified
-          consultants.
+          Mudra Global has been formed to work progressively in the field of import & export,
+          business. We offer a secure platform to facilitate the supply of quality products across
+          domestic and foreign countries. As an import & export. business, we seek to provide real
+          services to exporters and importers who are committed to the worldwide supply of
+          commodities around the entire world. We deal reasonably with producers, farmers,
+          wholesalers, importers, and other stakeholders to establish a strong global presence in
+          international trade right away. Being an import export company, our final aspiration is to
+          close the gap in the foreign market between buyers and sellers. In order to offer the
+          highest quality of merchandise, ensure customer satisfaction and cultivate customer
+          loyalty, we guarantee assistance by qualified consultants.
         </p>
       </div>
       <div className="w-full lg:basis-[30%] md:basis-[40%] my-auto md:block hidden">
