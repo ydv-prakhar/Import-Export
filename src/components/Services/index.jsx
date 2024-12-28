665cd8bf31e357e11c4a8105e9bf18de59bc9574
mitesh darda <mitesh.darda.0@gmail.com>
2024-12-28 17:48:02 +0530

    merge from main

--- src/components/Services/index.jsx
+++ src/components/Services/index.jsx
@@ -1,10 +1,10 @@
-import ServiceCard from "./ServiceCard"
+import ServiceCard from './components/ServiceCard';
 
 const Services = () => {
   return (
     <div className="m-12 mx-[6%]">
       <div className="text-center text-[20px] my-[20px] font-[400] underline underline-offset-[20px] decoration-orange-500">
-        Mundra Global Services{" "}
+        Mundra Global Services{' '}
       </div>
 
       <div className="flex gap-[2.5%] flex-wrap mt-12">
@@ -24,10 +24,9 @@ const Services = () => {
           heading="Import Export"
           text="Mudra Global is involved in the import export of variety of food and commodity around the world."
         />
-        
       </div>
     </div>
   );
-}
+};
 
-export default Services
+export default Services;
