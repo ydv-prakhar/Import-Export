93ba884d2f63e2186e1b750e4865565a9fce2bc3
Prakhar Yadav <yadavprakhar1809@gmail.com>
2024-12-28 16:30:52 +0530

    feat:Services Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import Card from "./components/Card";
 import { ProductCategories } from "./components/ProductCategories";
+import Services from "./components/Services";
 
 function App() {
   return (
     <>
-      <ProductCategories />
+      <Services />
     </>
   );
 }
