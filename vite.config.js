5a8890ea6516ef770fe398942319baebc7fdb79e
mitesh darda <mitesh.darda.0@gmail.com>
2024-12-28 16:30:03 +0530

    refact: update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:3000'
+  },
+  preview: {
+    port: 3000,
+    strictPort: true
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
+  }
+});
