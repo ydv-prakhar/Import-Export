9ac1a51c15fb09625561f484528b28da3751435e
mitesh darda <mitesh.darda.0@gmail.com>
2024-12-29 11:59:35 +0530

    fix: video componet

---
+++ src/components/Testimonial/index.jsx
@@ -0,0 +1,19 @@
+const Testimonial = () => {
+  return (
+    <div className="p-12 flex flex-col bg-[#9cbf60] text-white ">
+      <div className="flex justify-center">
+        <img src="public\PARAMJIT.jpg" alt="" className="rounded-[50%]" />
+      </div>
+      <div className="text-white text-[18px] tracking-widest w-[80%] p-5 mx-auto text-center">
+        “A professional & an entrepreneur with over 25 years of experience, including general
+        management of small to large size organizations; corporate development, business operations,
+        complete product marketing, and strategy across the globe.
+      </div>
+      <div className="tracking-wider text-[18px] text-white font-[600] text-center textmy-5">
+        MR. PARAMJIT ANAND, DIRECTOR
+      </div>
+    </div>
+  );
+};
+
+export default Testimonial;
