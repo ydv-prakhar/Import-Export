d6e06313b1e237e9018f322d58f9346ab4ca4aae
Prakhar Yadav <yadavprakhar1809@gmail.com>
2025-01-01 23:54:13 +0530

    feat:added products

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,8 +3,8 @@
 const Card = (props) => {
   return (
     <div className="flex w-[80%] md:w-[45%] lg:w-[25%] flex-col text-center text-[18px] font-[600] m-2">
-      <div className="w-full">
-        <img src={props.image} alt="" className="w-full" />
+      <div className="w-full h-full">
+        <img src={props.image} alt="" className="w-full h-full" />
       </div>
       <div className="my-[24px]">{props.text}</div>
     </div>
--- src/components/ProductCategories/index.jsx
+++ src/components/ProductCategories/index.jsx
@@ -9,15 +9,15 @@ const ProductCategories = () => {
       </div>
 
       <div className="flex flex-wrap mt-5 gap-[10%]">
-        <Card image="assets/image1.jpg" text="AGRO-COMMODITIES" />
-        <Card image="assets/image1.jpg" text="AGRO-COMMODITIES" />
-        <Card image="assets/image1.jpg" text="AGRO-COMMODITIES" />
-        <Card image="assets/image1.jpg" text="AGRO-COMMODITIES" />
-        <Card image="assets/image1.jpg" text="AGRO-COMMODITIES" />
-        <Card image="assets/image1.jpg" text="AGRO-COMMODITIES" />
-        <Card image="assets/image1.jpg" text="AGRO-COMMODITIES" />
-        <Card image="assets/image1.jpg" text="AGRO-COMMODITIES" />
-        <Card image="assets/image1.jpg" text="AGRO-COMMODITIES" />
+        <Card image="assets/image1.jpg" text="Agri & Food Products" />
+        <Card image="assets/pexels-pixabay-264636.jpg" text="Green vegitables & Fruits" />
+        <Card image="assets\premium_photo-1668487826871-2f2cac23ad56.avif" text="Medicine" />
+        <Card image="assets/EngineeringnMachinery.jpeg" text="Engineering and Machinery" />
+        <Card image="assets/Plastic Drums.jpg" text="Plastic Containers/Furniture" />
+        <Card image="assets/Chemicals.jpg" text="Chemicals and Cleaning agents" />
+        <Card image="assets/Jute bags.jpeg" text="Hand/Carry Bags" />
+        <Card image="assets/footwear.jpeg" text="Footwear" />
+        <Card image="assets/Bamboo furniture.jpeg" text="Furniture (Bamboo)" />
       </div>
     </div>
   );
