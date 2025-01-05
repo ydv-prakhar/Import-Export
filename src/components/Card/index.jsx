fb2e2873fc24bdba4434f4dd74bb5aecd314d666
Prakhar Yadav <yadavprakhar1809@gmail.com>
2025-01-05 14:00:05 +0530

    fix:Image height

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -4,7 +4,7 @@ const Card = (props) => {
   return (
     <div className="flex w-[80%] md:w-[45%] lg:w-[25%] flex-col text-center text-[18px] font-[600] m-2">
       <div className="w-full h-full">
-        <img src={props.image} alt="" className="w-full h-full" />
+        <img src={props.image} alt="" className="w-full h-[200px]" />
       </div>
       <div className="my-[24px]">{props.text}</div>
     </div>
